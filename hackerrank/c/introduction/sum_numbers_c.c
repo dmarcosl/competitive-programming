c377de79f976be19795196b7747580b0de37f662
Daniel Marcos <danielmarcos.to@gmail.com>
2019-05-22 21:14:53 +0200

    [hackerrank] Add solutions for C introduction

---
+++ hackerrank/c/introduction/sum_numbers_c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	int i1, i2;
+    float f1, f2;
+
+    scanf("%i%i%f%f", &i1, &i2, &f1, &f2);
+    printf("%i %i\n%.1f %.1f", (i1 + i2), (i1 - i2), (f1 + f2), (f1 - f2));
+
+    return 0;
+}
+
